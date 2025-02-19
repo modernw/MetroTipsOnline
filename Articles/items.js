c9d745772188de3fc789adef694608ef3f2e9dff
HollowAware <103006798+WinterSamu@users.noreply.github.com>
2025-02-19 15:40:19 +0800

    Update items.js

--- Articles/items.js
+++ Articles/items.js
@@ -2,7 +2,7 @@ var data = [
 	{
 		"title": "如何获取商店应用的安装包",
 		"url": "Articles/GetApp/Index.html",
-		"background": "Images/win10-hero-logon.png"
+		"background": "Images/win10-hero-logon.jpg"
 	},
 	{
 		"title": "如何安装商店应用的安装包",
@@ -17,32 +17,25 @@ var data = [
 	{
 		"title": "结语",
 		"url": "Articles/End/Index.html",
-		"background": "Images/kagamine-rin-and-len.png"
+		"background": "Images/kagamine-rin-and-len.jpg"
 	},
 ];
 
 function loadItems() {  
 	const listView = document.getElementById("listView");  
-	listView.innerHTML = ''; // 清空现有内容以避免重复  
-  
-	// 使用 for 循环代替 forEach  
+	listView.innerHTML = ''; 
 	for (let i = 0; i < data.length; i++) {  
 		const item = data[i];  
 		const listItem = document.createElement("div");  
 		listItem.className = "listView-item win-item";  
-		//listItem.style.backgroundImage = `url(${item.background})`;  
 		listItem.style.backgroundImage = 'url(' + item.background + ')';
-
 		const title = document.createElement("div");  
 		title.className = "title";  
 		title.textContent = item.title;  
-  
-		listItem.appendChild(title);  
-		// 添加点击事件监听器  
+		listItem.appendChild(title);   
 		listItem.addEventListener("click", function() {  
 			showContent(item.url, item.background);  
 		});  
-  
 		listView.appendChild(listItem);  
 	}  
 }  
